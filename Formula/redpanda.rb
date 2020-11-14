5774bd456587c3759f32daf0ec2167dbd4c38924
Roko Kruze <rkruze@gmail.com>
2020-11-13 16:12:16 -0800

    Initial Commit

--- Formula/redpanda.rb
+++ Formula/redpanda.rb
@@ -12,7 +12,25 @@ class Redpanda < Formula
     end
 
     def caveats; <<~EOS
-        These are the different caveats we should list and also how to use it. 
+        Redpanda - The fastest queue in the west!
+
+        This installs RPK which, with Docker, enables the running of local cluster 
+        for testing purposes.
+
+        You can start a 3 node cluster locally using the following command: 
+
+            rpk container start -n 3
+        
+        You can then interact with the cluster using commands like the following: 
+
+            rpk api status
+        
+        When done, you can stop and delete the cluster with the following command:
+            
+            rpk container purge
+
+        For information on how to setup production evironments, check out our
+        installation guide here: https://vectorized.io/documentation/setup-guide/
     EOS
     end
 end
---
+++ README.md
@@ -0,0 +1,25 @@
+# Redpanda [Homebrew] Tap
+
+This is the official source for installing [Redpanda] with Homebrew.
+
+## Installing
+
+```shell
+$ brew install vectorizedio/tap/redpanda
+```
+
+Alternately, you can configure the tap and install the package separately:
+
+``` shell
+$ brew tap vectorizedio/tap
+$ brew install redpanda
+```
+
+Please referrer to our complete [Documentation] on how to install Redpanda on other environments. 
+Find further documentation on installing Redpanda here
+See our complete [installation instructions] for alternative installation
+methods, including support for Linux and Windows.
+
+[Redpanda]: https://vectorized.io
+[Homebrew]: https://brew.sh
+[Documentation]: https://vectorized.io/documentation
